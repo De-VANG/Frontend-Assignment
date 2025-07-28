5d1e220ae740941fcd3372d7f76360fe9cf46dd6
De-VANG <devangbhatnagar12@gmail.com>
2025-07-28 08:24:05 +0530

    Updated the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
-import SignUp from './page/signup.jsx';
+import Navigationbar from './components/navbar.jsx'
+import SignUp from './page/signup.jsx'
+import UserCard from './components/usercard.jsx'
+
 import './App.css'
 
 function App() {
 
+  const[isSignedUp, setIsSignedUp] = useState(false)
+  const[data, setData] = useState({})
+
   return (
-    <div className="App">
-      <SignUp />
-    </div>
+    <div className="App">{
+      !isSignedUp ? (
+      <SignUp onSignUp={(data) => {
+          setData(data)
+          setIsSignedUp(true)
+        }} />
+      ):(
+        <>
+        <Navigationbar />
+
+        <div className="p-10 px-0">
+          <h1 className="text-2xl  mb-4 break-words ">Welcome, {data.fullName}!</h1>
+          <div className ="flex justify-center items-start min-h-screen rounded-3xl bg-gray-100 pt-20">
+            
+          <UserCard name={data.fullName} email={data.email} />
+          </div>
+          </div>
+          </>
+      )}
+      </div>
+      
   )
 }
 
